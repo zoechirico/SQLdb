70383388ed9297d63a34da8c15ca48f148207574
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-19 11:09:42 -0500

    Initial Commit

---
+++ SQLdb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SQLdb
+//
+//  Created by Mike Chirico on 12/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SQLdb/SQLdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLdbApp.swift
+//  SQLdb
+//
+//  Created by Mike Chirico on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SQLdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
